28612295e8bdd4d1ca2a4104848f0f98a6656302
NicolaTea <teanicola2004@gmail.com>
2024-10-10 12:18:25 +0300

    auf1+testing auf1 si auf2

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,2 +1,64 @@
 public class Aufgabe1 {
+    //1
+    public static int[] NichtAusreichend(int[] noten){
+        //numaram notele<40
+        int counter=0;
+        for(int note:noten){
+            if(note<40){
+                counter++;
+            }
+        }
+        //creeam un array cu notele insuficiente
+        int[] nichtausreichend=new int[counter];
+        int idx=0;
+        for(int note:noten){
+            if(note<40){
+                nichtausreichend[idx++]=note;
+            }
+        }
+        return nichtausreichend;
+    }
+
+    //2
+    public static double Durchschnittwert(int[] noten){
+        int sum=0;
+        for(int note:noten){
+            sum+=note;
+        }
+        return Math.round((double)sum/noten.length*100)/100; //calcul+rotunjire 2 decimale
+    }
+
+    //3
+    public static int[] AbgerundeteNoten(int[] noten){
+        int[] abgerundet=new int[noten.length];
+        for(int i=0;i<noten.length;i++){
+            abgerundet[i]=RundeNote(noten[i]);
+        }
+        return abgerundet;
+
+    }
+
+    public static int RundeNote(int note){ //metoda aux
+        if(note<38){
+            return note;
+        }
+        int diff=5-(note%5);
+        if(diff<3){
+            return note+diff;  //rotunjim la urm. multiplu de 5
+        }
+        return note;
+    }
+
+    //4
+    public static int MaximalAbgerundeteNote(int [] noten){
+        int max=0;
+        int[] abgerundet=AbgerundeteNoten(noten);
+        for(int note: abgerundet){
+            if(note>max){
+                max=note;
+            }
+        }
+        return max;
+    }
+
 }
---
+++ src/Testing.java
@@ -0,0 +1,49 @@
+public class Testing {
+    public static void main(String args[]){
+            //auf1
+            int[] note = {29, 37, 38, 41, 84, 67};
+
+            // 1. Note insuficiente
+            int[] insuficiente = Aufgabe1.NichtAusreichend(note);
+            System.out.print("Note insuficiente: ");
+            for (int nota : insuficiente) {
+                    System.out.print(nota + " ");
+            }
+            System.out.println();
+
+            // 2. Valoare medie..ceva nu iasa
+            double medie = Aufgabe1.Durchschnittwert(note);
+            System.out.println("Valoarea medie: " + medie);
+
+            // 3. Note rotunjite
+            int[] rotunjite = Aufgabe1.AbgerundeteNoten(note);
+            System.out.print("Note rotunjite: ");
+            for (int nota : rotunjite) {
+                    System.out.print(nota + " ");
+            }
+            System.out.println();
+
+            // 4. Nota maxima rotunjita
+            int maxRotunjit = Aufgabe1.MaximalAbgerundeteNote(note);
+            System.out.println("Nota maxima rotunjita: " + maxRotunjit);
+
+            System.out.println("-----------------------------------------------");
+
+            //auf2
+            int number[]={4,8,3,10,17};
+            //1
+            int max=Aufgabe2.maxim(number);
+            System.out.println("Valoarea maxima: "+max);
+            //2
+            int min=Aufgabe2.minim(number);
+            System.out.println("Valoarea minima: "+min);
+            //3
+            int max_sum=Aufgabe2.maximalSumme(number);
+            System.out.println("Suma maxima: "+max_sum);
+            //4
+            int min_sum=Aufgabe2.minimalSumme(number);
+            System.out.println("Suma minima: "+min_sum);
+
+            System.out.println("-----------------------------------------------");
+    }
+}
